b5d53aa136817d8566291a6c5b4ae4e71428d700
eno2kn <92361246+eno2kn@users.noreply.github.com>
2023-12-27 23:52:45 +0900

    feat: add npm api (#1)
    * add npm api
    
    * add badges
    
    * add cache
    
    * remove unused files
    
    * fix badge url
    
    * add `memo()`

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,79 +1,109 @@
-import { useState } from 'react';
-import {
-  ContributorsResult,
-  DownloadCountResult,
-  LatestVersionPublishedAtResult,
-} from './result';
-import {
-  getNpmPackage,
-  parsePackageJSONRepository,
-  useNpmPackage,
-  useNpmPackageDownloadCount,
-} from '../lib/npm-registry';
-import { useGitHubRepoContributors } from '../lib/github';
+import { useCallback, useMemo, useState } from 'react';
+import { hc } from 'hono/client';
+import { AppType } from '../api';
+import { Result, Status } from './result';
+import { Badges } from './badge';
+
+type NpmPkgData = {
+  latest:
+    | {
+        version: string;
+        publishedAt: string;
+      }
+    | undefined;
+  downloads: number | undefined;
+  contributors: number | undefined;
+};
+type StatusWithoutLoading = Exclude<Status, 'loading'>;
+
+function calcStatus<T>(
+  value: T | undefined,
+  checkError: (v: T) => boolean,
+): StatusWithoutLoading {
+  if (value) {
+    if (checkError(value)) {
+      return 'error';
+    } else {
+      return 'success';
+    }
+  } else {
+    return 'initial';
+  }
+}
 
 export const App: React.FC = () => {
   const [inputPkgName, setInputPkgName] = useState('');
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [pkgName, setPkgName] = useState('');
 
-  const {
-    isLoading: isNpmPkgLoading,
-    data: npmPkg,
-    mutate: mutateNpmPkg,
-    reset: resetNpmPkg,
-  } = useNpmPackage();
-  const {
-    isLoading: isNpmPkgDownloadCountLoading,
-    data: npmPkgDownloadCount,
-    mutate: mutateNpmPkgDownloadCount,
-    reset: resetNpmPkgDownloadCount,
-  } = useNpmPackageDownloadCount();
-  const {
-    isLoading: isContributorsLoading,
-    data: contributors,
-    mutate: mutateContributors,
-    reset: resetContributors,
-  } = useGitHubRepoContributors();
+  const [data, setData] = useState<NpmPkgData>({
+    downloads: undefined,
+    contributors: undefined,
+    latest: undefined,
+  });
+
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const downloadsStatus = useMemo(
+    () => calcStatus(data.downloads, (v) => v < 1000),
+    [data],
+  );
+  const contributorsStatus = useMemo(
+    () => calcStatus(data.contributors, (v) => v < 3),
+    [data],
+  );
+  const latestStatus = useMemo(
+    () =>
+      calcStatus(
+        data.latest,
+        (v) =>
+          Date.now() - new Date(v.publishedAt).getTime() >
+          1000 * 60 * 60 * 24 * 365,
+      ),
+    [data],
+  );
+
+  const getStatus = useCallback(
+    (st: Exclude<Status, 'loading'>): Status => {
+      return isLoading ? 'loading' : st;
+    },
+    [isLoading],
+  );
+
+  const client = hc<AppType>('/');
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!inputPkgName) {
+    if (!inputPkgName) return;
+
+    setLoading(true);
+    setError(false);
+    setPkgName('');
+    setData({
+      latest: undefined,
+      downloads: undefined,
+      contributors: undefined,
+    });
+
+    const res = await client.api.npm.$get({
+      query: { name: inputPkgName },
+    });
+
+    if (!res.ok) {
+      setLoading(false);
+      setError(true);
       return;
     }
-    setErrorMessage(null);
+    setPkgName(inputPkgName);
 
-    // npm registry APIからパッケージについての情報を取得する
-    const npmPackageInfo = await getNpmPackage(inputPkgName);
-    // npmに該当するパッケージがない
-    if (!npmPackageInfo) {
-      setErrorMessage('指定されたパッケージが存在しません');
-      resetNpmPkgDownloadCount();
-      resetContributors();
-      resetNpmPkg();
-      return;
-    }
-    const pkgName = inputPkgName;
+    const json = await res.json();
+    const { latest, downloads, contributors } = json;
 
-    const promises: unknown[] = [
-      // 最新バージョンの公開日をチェック
-      await mutateNpmPkg(npmPackageInfo),
-      // ここ1ヶ月の総ダウンロード数を取得する
-      await mutateNpmPkgDownloadCount(pkgName),
-    ];
-
-    // githubリポジトリのURLを取得する
-    const githubRepo = parsePackageJSONRepository(npmPackageInfo);
-    if (!githubRepo) {
-      // package.jsonにrepositoryの情報がない
-      setErrorMessage('package.jsonにrepositoryの情報がありません');
-      resetContributors();
-    } else {
-      const { owner, repo } = githubRepo;
-
-      // コントリビューター数をチェック
-      promises.push(await mutateContributors(owner, repo));
-    }
-
-    Promise.allSettled(promises);
+    setData({
+      latest,
+      downloads,
+      contributors,
+    });
+    setLoading(false);
   };
 
   return (
@@ -89,28 +119,43 @@ export const App: React.FC = () => {
           <button
             type="submit"
             className="border-l border-gray-300 whitespace-nowrap px-3"
+            disabled={!inputPkgName}
           >
             チェック
           </button>
         </div>
       </form>
-      {errorMessage && (
+      {isError && (
         <div className="mb-6 p-4 rounded border border-red-300 bg-red-50">
-          {errorMessage}
+          エラーが発生しました
+        </div>
+      )}
+      {!isLoading && pkgName && (
+        <div className="mb-6">
+          <div className="mb-2 text-lg font-semibold">{pkgName}</div>
+          <Badges name={pkgName} />
         </div>
       )}
       <div className="flex flex-col gap-4">
-        <DownloadCountResult
-          loading={isNpmPkgDownloadCountLoading}
-          downloadCount={npmPkgDownloadCount}
+        <Result
+          label="1ヶ月のダウンロード数"
+          value={data.downloads}
+          status={getStatus(downloadsStatus)}
         />
-        <ContributorsResult
-          loading={isContributorsLoading}
-          contributors={contributors}
+        <Result
+          label="Contributorの数"
+          value={data.contributors}
+          status={getStatus(contributorsStatus)}
         />
-        <LatestVersionPublishedAtResult
-          loading={isNpmPkgLoading}
-          npmPackage={npmPkg}
+        <Result
+          label="最新バージョンのリリース日"
+          value={
+            data.latest &&
+            `v${data.latest.version} ${new Date(
+              data.latest.publishedAt,
+            ).toLocaleDateString('ja-JP')}`
+          }
+          status={getStatus(latestStatus)}
         />
       </div>
     </div>
---
+++ src/components/badge.tsx
@@ -0,0 +1,67 @@
+import { memo } from 'react';
+
+export const NpmVersionBadge: React.FC<{
+  name: string;
+}> = ({ name }) => {
+  return (
+    <a
+      href={`https://www.npmjs.com/package/${name}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={`https://badge.fury.io/js/${encodeURIComponent(name)}.svg`}
+        alt="npm version"
+        referrerPolicy="no-referrer"
+      />
+    </a>
+  );
+};
+
+export const SocketBadge: React.FC<{
+  name: string;
+}> = ({ name }) => {
+  return (
+    <a
+      href={`https://socket.dev/npm/package/${name}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={`https://socket.dev/api/badge/npm/package/${name}`}
+        alt="socket"
+        referrerPolicy="no-referrer"
+      />
+    </a>
+  );
+};
+
+export const SnykBadge: React.FC<{
+  name: string;
+}> = ({ name }) => {
+  return (
+    <a
+      href={`https://snyk.io/advisor/npm-package/${name}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src={`https://snyk.io//advisor/npm-package/${name}/badge.svg`}
+        alt="snyk"
+        referrerPolicy="no-referrer"
+      />
+    </a>
+  );
+};
+
+export const Badges: React.FC<{
+  name: string;
+}> = memo(function Badges({ name }) {
+  return (
+    <div className="flex gap-2">
+      <NpmVersionBadge name={name} />
+      <SocketBadge name={name} />
+      <SnykBadge name={name} />
+    </div>
+  );
+});
